a81d65fc02175588c15464cf997b38815c492460
Elian Jaramillo <jaramilloelian811@gmail.com>
2022-10-18 15:03:07 -0500

    declaracion

---
+++ impressCrush.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+x= range(2)
+
+for n in x:
+    color("red")
+    begin_fill()
+    pensize(5)
+    left(50)
+    forward(155)
+    circle(85,200)
+    forward(155)
+    end_fill()
+    
+
